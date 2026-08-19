7fb670c5713864d34c4634c88e04e71ac7a65bac
trashdb <trashdbapp@gmail.com>
2026-08-19 09:53:11 +0200

    Fix native menu bar icon rendering

--- native/Views/MenuBarLabelView.swift
+++ native/Views/MenuBarLabelView.swift
@@ -1,12 +1,46 @@
+import AppKit
 import SwiftUI
+
+private enum MenuBarIcon {
+    static let image: NSImage = {
+        let image = NSImage(size: NSSize(width: 18, height: 18), flipped: false) { rect in
+            let scale = rect.width / 24
+            let height = rect.height
+            let path = NSBezierPath()
+
+            path.move(to: NSPoint(x: 3 * scale, y: height - 9 * scale))
+            path.curve(
+                to: NSPoint(x: 10 * scale, y: height - 9 * scale),
+                controlPoint1: NSPoint(x: 5.3 * scale, y: height - 6.7 * scale),
+                controlPoint2: NSPoint(x: 7.7 * scale, y: height - 6.7 * scale)
+            )
+            path.line(to: NSPoint(x: 14 * scale, y: height - 13 * scale))
+            path.curve(
+                to: NSPoint(x: 21 * scale, y: height - 13 * scale),
+                controlPoint1: NSPoint(x: 16.3 * scale, y: height - 15.3 * scale),
+                controlPoint2: NSPoint(x: 18.7 * scale, y: height - 15.3 * scale)
+            )
+
+            NSColor.black.setStroke()
+            path.lineWidth = 2.2
+            path.lineCapStyle = .round
+            path.stroke()
+            return true
+        }
+        image.isTemplate = true
+        return image
+    }()
+}
 
 struct MenuBarLabelView: View {
     @ObservedObject private var badge = MenuBarBadgeService.shared
 
     var body: some View {
-        WaveMark(color: .primary)
+        Image(nsImage: MenuBarIcon.image)
+            .renderingMode(.template)
             .frame(width: 18, height: 18)
             .help(tooltip)
+            .accessibilityLabel(tooltip)
     }
 
     private var tooltip: String {
